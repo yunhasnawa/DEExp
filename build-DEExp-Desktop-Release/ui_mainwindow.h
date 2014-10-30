19e464bd9226acf87143835837c304582254588a
Yoppy Yunhasnawa <yunhasnawa@gmail.com>
2014-10-31 01:37:55 +0800

    Fix some misplaced labels

---
+++ build-DEExp-Desktop-Release/ui_mainwindow.h
@@ -0,0 +1,147 @@
+/********************************************************************************
+** Form generated from reading UI file 'mainwindow.ui'
+**
+** Created by: Qt User Interface Compiler version 5.2.1
+**
+** WARNING! All changes made in this file will be lost when recompiling UI file!
+********************************************************************************/
+
+#ifndef UI_MAINWINDOW_H
+#define UI_MAINWINDOW_H
+
+#include <QtCore/QVariant>
+#include <QtWidgets/QAction>
+#include <QtWidgets/QApplication>
+#include <QtWidgets/QButtonGroup>
+#include <QtWidgets/QHeaderView>
+#include <QtWidgets/QLabel>
+#include <QtWidgets/QMainWindow>
+#include <QtWidgets/QMenu>
+#include <QtWidgets/QMenuBar>
+#include <QtWidgets/QPushButton>
+#include <QtWidgets/QStatusBar>
+#include <QtWidgets/QToolBar>
+#include <QtWidgets/QWidget>
+
+QT_BEGIN_NAMESPACE
+
+class Ui_MainWindow
+{
+public:
+    QAction *actionRead_Data;
+    QAction *actionWrite_Data;
+    QAction *actionFile_Operation;
+    QWidget *centralWidget;
+    QPushButton *pbWriteData;
+    QPushButton *pbReadData;
+    QPushButton *pbFileOps;
+    QLabel *label;
+    QLabel *label_2;
+    QLabel *label_3;
+    QLabel *label_4;
+    QLabel *label_5;
+    QMenuBar *menuBar;
+    QMenu *menuMenu;
+    QToolBar *mainToolBar;
+    QStatusBar *statusBar;
+
+    void setupUi(QMainWindow *MainWindow)
+    {
+        if (MainWindow->objectName().isEmpty())
+            MainWindow->setObjectName(QStringLiteral("MainWindow"));
+        MainWindow->resize(571, 387);
+        actionRead_Data = new QAction(MainWindow);
+        actionRead_Data->setObjectName(QStringLiteral("actionRead_Data"));
+        actionWrite_Data = new QAction(MainWindow);
+        actionWrite_Data->setObjectName(QStringLiteral("actionWrite_Data"));
+        actionFile_Operation = new QAction(MainWindow);
+        actionFile_Operation->setObjectName(QStringLiteral("actionFile_Operation"));
+        centralWidget = new QWidget(MainWindow);
+        centralWidget->setObjectName(QStringLiteral("centralWidget"));
+        pbWriteData = new QPushButton(centralWidget);
+        pbWriteData->setObjectName(QStringLiteral("pbWriteData"));
+        pbWriteData->setGeometry(QRect(150, 140, 80, 61));
+        pbReadData = new QPushButton(centralWidget);
+        pbReadData->setObjectName(QStringLiteral("pbReadData"));
+        pbReadData->setGeometry(QRect(250, 140, 80, 61));
+        pbFileOps = new QPushButton(centralWidget);
+        pbFileOps->setObjectName(QStringLiteral("pbFileOps"));
+        pbFileOps->setGeometry(QRect(350, 140, 80, 61));
+        label = new QLabel(centralWidget);
+        label->setObjectName(QStringLiteral("label"));
+        label->setGeometry(QRect(20, 290, 301, 16));
+        label_2 = new QLabel(centralWidget);
+        label_2->setObjectName(QStringLiteral("label_2"));
+        label_2->setGeometry(QRect(110, 310, 451, 16));
+        QFont font;
+        font.setBold(true);
+        font.setWeight(75);
+        label_2->setFont(font);
+        label_3 = new QLabel(centralWidget);
+        label_3->setObjectName(QStringLiteral("label_3"));
+        label_3->setGeometry(QRect(110, 290, 451, 16));
+        label_3->setFont(font);
+        label_4 = new QLabel(centralWidget);
+        label_4->setObjectName(QStringLiteral("label_4"));
+        label_4->setGeometry(QRect(120, 80, 331, 20));
+        QFont font1;
+        font1.setItalic(true);
+        label_4->setFont(font1);
+        label_5 = new QLabel(centralWidget);
+        label_5->setObjectName(QStringLiteral("label_5"));
+        label_5->setGeometry(QRect(70, 30, 421, 20));
+        QFont font2;
+        font2.setPointSize(14);
+        font2.setBold(true);
+        font2.setWeight(75);
+        label_5->setFont(font2);
+        MainWindow->setCentralWidget(centralWidget);
+        menuBar = new QMenuBar(MainWindow);
+        menuBar->setObjectName(QStringLiteral("menuBar"));
+        menuBar->setGeometry(QRect(0, 0, 571, 20));
+        menuMenu = new QMenu(menuBar);
+        menuMenu->setObjectName(QStringLiteral("menuMenu"));
+        MainWindow->setMenuBar(menuBar);
+        mainToolBar = new QToolBar(MainWindow);
+        mainToolBar->setObjectName(QStringLiteral("mainToolBar"));
+        MainWindow->addToolBar(Qt::TopToolBarArea, mainToolBar);
+        statusBar = new QStatusBar(MainWindow);
+        statusBar->setObjectName(QStringLiteral("statusBar"));
+        MainWindow->setStatusBar(statusBar);
+
+        menuBar->addAction(menuMenu->menuAction());
+        menuMenu->addAction(actionRead_Data);
+        menuMenu->addAction(actionWrite_Data);
+        menuMenu->addAction(actionFile_Operation);
+
+        retranslateUi(MainWindow);
+
+        QMetaObject::connectSlotsByName(MainWindow);
+    } // setupUi
+
+    void retranslateUi(QMainWindow *MainWindow)
+    {
+        MainWindow->setWindowTitle(QApplication::translate("MainWindow", "DEExp - Data Engineering Experiments", 0));
+        actionRead_Data->setText(QApplication::translate("MainWindow", "Read Data", 0));
+        actionWrite_Data->setText(QApplication::translate("MainWindow", "Write Data", 0));
+        actionFile_Operation->setText(QApplication::translate("MainWindow", "File Operation", 0));
+        pbWriteData->setText(QApplication::translate("MainWindow", "Write Data", 0));
+        pbReadData->setText(QApplication::translate("MainWindow", "Read Data", 0));
+        pbFileOps->setText(QApplication::translate("MainWindow", "File Ops", 0));
+        label->setText(QApplication::translate("MainWindow", "Created By: ", 0));
+        label_2->setText(QApplication::translate("MainWindow", "Yoppy Yunhasnawa (M0329016)", 0));
+        label_3->setText(QApplication::translate("MainWindow", "Toga Aldila Cinderatama (M0329011)", 0));
+        label_4->setText(QApplication::translate("MainWindow", "Click one or more buttons below or choose from Menu", 0));
+        label_5->setText(QApplication::translate("MainWindow", "Data Engineering Class Assignment #1", 0));
+        menuMenu->setTitle(QApplication::translate("MainWindow", "Menu", 0));
+    } // retranslateUi
+
+};
+
+namespace Ui {
+    class MainWindow: public Ui_MainWindow {};
+} // namespace Ui
+
+QT_END_NAMESPACE
+
+#endif // UI_MAINWINDOW_H
---
+++ build-DEExp-Desktop-Release/ui_writedatadialog.h
@@ -0,0 +1,119 @@
+/********************************************************************************
+** Form generated from reading UI file 'writedatadialog.ui'
+**
+** Created by: Qt User Interface Compiler version 5.2.1
+**
+** WARNING! All changes made in this file will be lost when recompiling UI file!
+********************************************************************************/
+
+#ifndef UI_WRITEDATADIALOG_H
+#define UI_WRITEDATADIALOG_H
+
+#include <QtCore/QVariant>
+#include <QtWidgets/QAction>
+#include <QtWidgets/QApplication>
+#include <QtWidgets/QButtonGroup>
+#include <QtWidgets/QDialog>
+#include <QtWidgets/QDialogButtonBox>
+#include <QtWidgets/QHeaderView>
+#include <QtWidgets/QLabel>
+#include <QtWidgets/QLineEdit>
+#include <QtWidgets/QPushButton>
+#include <QtWidgets/QTextEdit>
+
+QT_BEGIN_NAMESPACE
+
+class Ui_WriteDataDialog
+{
+public:
+    QDialogButtonBox *buttonBox;
+    QLabel *label;
+    QLineEdit *ldtBytePowerSize;
+    QLineEdit *ldtFileName;
+    QPushButton *pbtWriteToDisk;
+    QLabel *label_2;
+    QLabel *label_4;
+    QPushButton *pbtGenerate;
+    QTextEdit *tedContent;
+    QLabel *label_3;
+    QLineEdit *ldtByteMainSize;
+    QLabel *label_5;
+    QLineEdit *ldtTotalTime;
+
+    void setupUi(QDialog *WriteDataDialog)
+    {
+        if (WriteDataDialog->objectName().isEmpty())
+            WriteDataDialog->setObjectName(QStringLiteral("WriteDataDialog"));
+        WriteDataDialog->resize(500, 430);
+        buttonBox = new QDialogButtonBox(WriteDataDialog);
+        buttonBox->setObjectName(QStringLiteral("buttonBox"));
+        buttonBox->setGeometry(QRect(10, 390, 481, 32));
+        buttonBox->setOrientation(Qt::Horizontal);
+        buttonBox->setStandardButtons(QDialogButtonBox::Cancel|QDialogButtonBox::Ok);
+        label = new QLabel(WriteDataDialog);
+        label->setObjectName(QStringLiteral("label"));
+        label->setGeometry(QRect(8, 10, 121, 16));
+        ldtBytePowerSize = new QLineEdit(WriteDataDialog);
+        ldtBytePowerSize->setObjectName(QStringLiteral("ldtBytePowerSize"));
+        ldtBytePowerSize->setGeometry(QRect(318, 10, 113, 23));
+        ldtFileName = new QLineEdit(WriteDataDialog);
+        ldtFileName->setObjectName(QStringLiteral("ldtFileName"));
+        ldtFileName->setGeometry(QRect(100, 310, 391, 23));
+        pbtWriteToDisk = new QPushButton(WriteDataDialog);
+        pbtWriteToDisk->setObjectName(QStringLiteral("pbtWriteToDisk"));
+        pbtWriteToDisk->setGeometry(QRect(158, 270, 181, 23));
+        label_2 = new QLabel(WriteDataDialog);
+        label_2->setObjectName(QStringLiteral("label_2"));
+        label_2->setGeometry(QRect(288, 10, 21, 16));
+        label_4 = new QLabel(WriteDataDialog);
+        label_4->setObjectName(QStringLiteral("label_4"));
+        label_4->setGeometry(QRect(8, 340, 81, 16));
+        pbtGenerate = new QPushButton(WriteDataDialog);
+        pbtGenerate->setObjectName(QStringLiteral("pbtGenerate"));
+        pbtGenerate->setGeometry(QRect(158, 50, 181, 23));
+        tedContent = new QTextEdit(WriteDataDialog);
+        tedContent->setObjectName(QStringLiteral("tedContent"));
+        tedContent->setGeometry(QRect(8, 80, 481, 181));
+        label_3 = new QLabel(WriteDataDialog);
+        label_3->setObjectName(QStringLiteral("label_3"));
+        label_3->setGeometry(QRect(8, 310, 81, 16));
+        ldtByteMainSize = new QLineEdit(WriteDataDialog);
+        ldtByteMainSize->setObjectName(QStringLiteral("ldtByteMainSize"));
+        ldtByteMainSize->setGeometry(QRect(158, 10, 113, 23));
+        label_5 = new QLabel(WriteDataDialog);
+        label_5->setObjectName(QStringLiteral("label_5"));
+        label_5->setGeometry(QRect(330, 340, 161, 16));
+        ldtTotalTime = new QLineEdit(WriteDataDialog);
+        ldtTotalTime->setObjectName(QStringLiteral("ldtTotalTime"));
+        ldtTotalTime->setGeometry(QRect(100, 340, 221, 23));
+
+        retranslateUi(WriteDataDialog);
+        QObject::connect(buttonBox, SIGNAL(accepted()), WriteDataDialog, SLOT(accept()));
+        QObject::connect(buttonBox, SIGNAL(rejected()), WriteDataDialog, SLOT(reject()));
+
+        QMetaObject::connectSlotsByName(WriteDataDialog);
+    } // setupUi
+
+    void retranslateUi(QDialog *WriteDataDialog)
+    {
+        WriteDataDialog->setWindowTitle(QApplication::translate("WriteDataDialog", "Dialog", 0));
+        label->setText(QApplication::translate("WriteDataDialog", "Data Size (Bytes)", 0));
+        ldtBytePowerSize->setText(QApplication::translate("WriteDataDialog", "8", 0));
+        pbtWriteToDisk->setText(QApplication::translate("WriteDataDialog", "Write to Disk", 0));
+        label_2->setText(QApplication::translate("WriteDataDialog", "^", 0));
+        label_4->setText(QApplication::translate("WriteDataDialog", "Total Time", 0));
+        pbtGenerate->setText(QApplication::translate("WriteDataDialog", "Generate!", 0));
+        label_3->setText(QApplication::translate("WriteDataDialog", "File name", 0));
+        ldtByteMainSize->setText(QApplication::translate("WriteDataDialog", "2", 0));
+        label_5->setText(QApplication::translate("WriteDataDialog", "Nano seconds", 0));
+    } // retranslateUi
+
+};
+
+namespace Ui {
+    class WriteDataDialog: public Ui_WriteDataDialog {};
+} // namespace Ui
+
+QT_END_NAMESPACE
+
+#endif // UI_WRITEDATADIALOG_H
